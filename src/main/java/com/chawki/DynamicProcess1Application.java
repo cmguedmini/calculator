22bf15ae67e59409147415e3fbb0d6a5a5cf3dda
Chawki Mguedmini <c.mguedmini@roam-smart.com>
2018-06-20 16:42:09 +0100

    Add spring boot skeleton

---
+++ src/main/java/com/chawki/DynamicProcess1Application.java
@@ -0,0 +1,12 @@
+package com.chawki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamicProcess1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DynamicProcess1Application.class, args);
+	}
+}
